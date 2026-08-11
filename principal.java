295ca44dcd20eb8478a5800ad89dc37e3f375dd2
bettegaa <bettegaheitor2@gmail.com>
2026-08-10 22:15:56 -0300

    ex2

--- principal.java
+++ principal.java
@@ -1,13 +1,28 @@
 public class Principal {
     public static void main(String[] args) {
-        Produto p1 = new Produto();
-        p1.nome = "Camiseta";
-        p1.preco = 29.90;
-        p1.estoque = 10;
+        Filme f1 = new Filme();
+        f1.titulo = "Matrix";
+        f1.duracao = 136;
+        f1.nota = 8.7;
+        f1.assistido = false;
 
-        p1.exibirInfo();
-        p1.vender(3);
-        p1.exibirInfo();
+        Filme f2 = new Filme();
+        f2.titulo = "Shrek";
+        f2.duracao = 90;
+        f2.nota = 7.9;
+        f2.assistido = false;
+
+        Filme f3 = new Filme();
+        f3.titulo = "Titanic";
+        f3.duracao = 195;
+        f3.nota = 7.8;
+        f3.assistido = false;
+
+        f1.marcarAssistido();
+        f2.avaliar(9.5);
+
+        f1.exibirFicha();
+        f2.exibirFicha();
+        f3.exibirFicha();
     }
-
 }
